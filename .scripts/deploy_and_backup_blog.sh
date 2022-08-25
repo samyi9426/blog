98efeceecb75cc173ec7f6905be590ee8dc18b97
zhongxm <samingzhong@foxmail.com>
2022-08-25 15:54:41 +0800

    自动同步blog

--- .scripts/deploy_and_backup_blog.sh
+++ .scripts/deploy_and_backup_blog.sh
@@ -4,9 +4,11 @@ echo "running script $0 at $(date '+%Y-%m-%d %H:%M:%S')..."
 
 cd $HOME/blog/
 # 部署
+echo "部署..."
 hexo clean
 hexo g -d
 
 # 备份
+echo "备份..."
 bash $HOME/blog/.scripts/autoUpload.sh
 echo -e "-----------------------------------------\n"
